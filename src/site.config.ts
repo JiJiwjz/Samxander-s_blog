b92abf99cd680d64c95185b5bbd941b9b004bbd4
卫佳喆 <weijiazhe123@mail.nwpu.edu.cn>
2026-03-21 15:28:31 +0800

    update: 2026-03-21 15:28

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: '卫佳喆 / Samxander',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: '专注 CV / Diffusion / 论文解读',
+  description: '专注 CV / Low-level Vision / 论文解读',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
