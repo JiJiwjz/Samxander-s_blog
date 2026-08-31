350416b731175779665dd0e96cebf69501b8d464
JiJiwjz <1983348796@qq.com>
2026-08-31 14:58:27 +0800

    feat:add-honors-page

--- src/site.config.ts
+++ src/site.config.ts
@@ -49,9 +49,8 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: 'Blog', link: '/blog' },
-      { title: 'Docs', link: '/docs' },
       { title: 'Projects', link: '/projects' },
-      { title: 'Links', link: '/links' },
+      { title: 'Honors', link: '/honors' },
       { title: 'About', link: '/about' }
     ]
   },
